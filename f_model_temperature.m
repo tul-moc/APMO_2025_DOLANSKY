68d00b3e045c1a6e7c7c95be70e9f7e34d12fee6
KarelDolansky <114815052+KarelDolansky@users.noreply.github.com>
2025-03-17 11:34:21 +0100

    function_temperature_view_model

---
+++ f_model_temperature.m
@@ -0,0 +1,16 @@
+function f_model_temperature(gd,sf,ns)
+model = createpde;
+g=decsg(gd,sf,ns);
+geometryFromEdges(model,g);
+
+applyBoundaryCondition(model,'dirichlet','Edge',1:3,'u',0);
+specifyCoefficients(model,'m',0,'d',0,'c',1,'a',0,'f',10);
+generateMesh(model);
+results = solvepde(model);
+pdeplot(model,'XYData',results.NodalSolution,'Mesh','on')
+
+
+% pdegplot(g,'EdgeLabels', 'on');
+% pdegplot(model,'EdgeLabels','on')
+
+end
