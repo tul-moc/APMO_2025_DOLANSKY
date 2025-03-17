de342a3f460330f788cd0664d859a798d91278d8
KarelDolansky <114815052+KarelDolansky@users.noreply.github.com>
2025-03-17 12:40:29 +0100

    model

---
+++ model_BracketWithHole.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+close all;
+model = femodel(AnalysisType="structuralStatic", ...
+                Geometry="BracketWithHole.stl");
+model.MaterialProperties = ...
+        materialProperties(YoungsModulus=200e9, ...
+                           PoissonsRatio=0.3);
+model.FaceBC(4) = faceBC(Constraint="fixed");
+model.FaceLoad(8) = faceLoad(SurfaceTraction=[0;0;-1e4]);
+model = generateMesh(model);
+result = solve(model)
+figure
+pdeplot3D(result.Mesh,ColorMapData=result.Displacement.ux);
+title("x-displacement")
+colormap("jet")
